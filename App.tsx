4afe50ee99481276e907010abdc562571b8e678b
00010216 <00010216@uca.edu.sv>
2020-10-16 16:11:23 -0600

    Bottom Navigation Tab added

--- App.tsx
+++ App.tsx
@@ -1,21 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+//Pantallas de prueba
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function MapScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Mapa</Text>
+    </View>
+  );
+}
+
+function DirectoryScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Directorio</Text>
+    </View>
+  );
+}
+
+function ContentScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Contenido</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function StackScreen() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'My home' }}
+      />
+      <Stack.Screen
+        name="Home"
+        component={MapScreen}
+        options={{ title: 'My home' }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+      
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home-variant' : 'home-variant';
+            } else if (route.name === 'Content') {
+              iconName = focused ? 'book-open-page-variant' : 'book-open-page-variant';
+            }else if (route.name === 'Directory') {
+              iconName = focused ? 'notebook' : 'notebook';
+            }else if (route.name === 'Escuelas') {
+              iconName = focused ? 'map-search' : 'map-search';
+            }
+
+            return <MaterialCommunityIcons containerStyle={{ marginTop: 2}} name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#00848c',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Content" component={ContentScreen} />
+        <Tab.Screen name="Directory" component={DirectoryScreen} />
+        <Tab.Screen name="Escuelas" component={MapScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
