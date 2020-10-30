aeba135f90c6998b37e0f75ea2ea4db62ac60019
00010216 <00010216@uca.edu.sv>
2020-10-29 22:30:33 -0600

    Created list component
    to work it has to get an array of the values

---
+++ src/components/list/List.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+    Image,
+    ImageSourcePropType,
+    StyleProp, FlatList, StyleSheet, Text, View, ViewStyle
+} from 'react-native';
+
+export interface TitleProps {
+    title: string
+}
+export interface ListProps {
+    data: Array<TitleProps>; //recibe arreglo de las categorias, o los contactos
+    style?: StyleProp<ViewStyle>;
+    onPress?: () => void;
+}
+
+export default function List(props: ListProps) {
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={props.data}
+                renderItem={({ item }) => <Text style={styles.item}>{item.title}</Text>}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+});
