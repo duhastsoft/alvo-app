f0357ba493e55c01ae9fa21be6fc76727bd0bef2
Kevin Velasquez <kevin.vela519@gmail.com>
2020-10-30 16:28:21 -0600

    Applied format to files

--- src/components/scroll-views/DirectoryScroll.tsx
+++ src/components/scroll-views/DirectoryScroll.tsx
@@ -1,55 +1,55 @@
 import React from 'react';
-import {ImageSourcePropType, ScrollView, StyleProp, ViewStyle, StyleSheet} from 'react-native';
+import { ImageSourcePropType, ScrollView, StyleProp, ViewStyle, StyleSheet } from 'react-native';
 import LoadingIcon from '@/assets/images/sand-clock-1.png';
 import ServiceCategory from '../card/DirectoryCard';
 
 interface DirectoryCard {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
-
 
 export interface DirectoryScrollProps {
-    image: ImageSourcePropType;
-    style?: StyleProp<ViewStyle>;
-    styleItems?: StyleProp<ViewStyle>;
-    onPressItem: (index: number) => void;
-    list: DirectoryCard[];
+  image: ImageSourcePropType;
+  style?: StyleProp<ViewStyle>;
+  styleItems?: StyleProp<ViewStyle>;
+  onPressItem: (index: number) => void;
+  list: DirectoryCard[];
 }
 
-export default function DirectoryScroll(props: DirectoryScrollProps){
-    if(props.list.length<1){
-        return(<ScrollView style={[styles.row, props.style]}>
-            <ServiceCategory
-                    index={0} 
-                    key={0} 
-                    image={LoadingIcon} 
-                    name={'CARGANDO...'}
-                    style={props.styleItems}
-                    />
-        </ScrollView>)
-    }
-    return(
-        <ScrollView style={[styles.row, props.style]}>
-            {
-                props.list.map((option)=>(<ServiceCategory
-                    index={option.id} 
-                    key={option.id} 
-                    image={props.image} 
-                    name={option.name}
-                    onPress={props.onPressItem}
-                    style={props.styleItems}
-                    />))
-            }
-        </ScrollView>
-    )
-    
+export default function DirectoryScroll(props: DirectoryScrollProps) {
+  if (props.list.length < 1) {
+    return (
+      <ScrollView style={[styles.row, props.style]}>
+        <ServiceCategory
+          index={0}
+          key={0}
+          image={LoadingIcon}
+          name={'CARGANDO...'}
+          style={props.styleItems}
+        />
+      </ScrollView>
+    );
+  }
+  return (
+    <ScrollView style={[styles.row, props.style]}>
+      {props.list.map((option) => (
+        <ServiceCategory
+          index={option.id}
+          key={option.id}
+          image={props.image}
+          name={option.name}
+          onPress={props.onPressItem}
+          style={props.styleItems}
+        />
+      ))}
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    row: {
-        width: '100%',
-        flexDirection: 'row',
-        marginBottom: 16,
-      },
-})
+  row: {
+    width: '100%',
+    flexDirection: 'row',
+    marginBottom: 16,
+  },
+});
--- src/screens/DirectoryScreen.tsx
+++ src/screens/DirectoryScreen.tsx
@@ -1,188 +1,192 @@
-import React from 'react';
-import {ActivityIndicator, View, StyleSheet, ImageSourcePropType, SafeAreaView, BackHandler} from 'react-native';
-import {Icon, SearchBar} from 'react-native-elements';
-import { StatusBar } from 'expo-status-bar';
 import BookMarkImage from '@/assets/images/bookmark-1.png';
 import CustomerSupport from '@/assets/images/customer-support-1.png';
-import Axios from 'axios';
 import DirectoryScroll from '@/components/scroll-views/DirectoryScroll';
-
+import Axios from 'axios';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { ActivityIndicator, BackHandler, SafeAreaView, StyleSheet, View } from 'react-native';
+import { Icon, SearchBar } from 'react-native-elements';
 
 interface ListItem {
-    name: string;
-    id: number;
+  name: string;
+  id: number;
 }
 
 const defaultCategory: ListItem = {
-    name: 'TODAS LAS CATEGORIAS',
-    id: 0
-}
+  name: 'TODAS LAS CATEGORIAS',
+  id: 0,
+};
 
-export default class DirectoryScreen extends React.Component{
-    constructor(props: {}){
-        super(props);
-        this.selectCategory = this.selectCategory.bind(this);
-        this.handleBackButtonClick = this.handleBackButtonClick.bind(this);
-    }
+export default class DirectoryScreen extends React.Component {
+  constructor(props: {}) {
+    super(props);
+    this.selectCategory = this.selectCategory.bind(this);
+    this.handleBackButtonClick = this.handleBackButtonClick.bind(this);
+  }
 
-    state = {
-        isLoading: true, 
-        search: '', 
-        section: 1,
+  state = {
+    isLoading: true,
+    search: '',
+    section: 1,
+    target: 0,
+    categories: [] as ListItem[],
+    dataSoruce: [] as ListItem[],
+  };
+
+  handleBackButtonClick() {
+    this.setState(
+      {
         target: 0,
-        categories: [] as ListItem[],
-        dataSoruce: [] as ListItem[]
-    };
+        section: 1,
+        isLoading: false,
+        categories: [defaultCategory],
+      },
+      () => this.loadContent()
+    );
+    return true;
+  }
 
+  selectCategory(target: number) {
+    this.setState(
+      {
+        target,
+        section: 2,
+        isLoading: false,
+        categories: [],
+      },
+      () => this.loadContent()
+    );
+  }
+  selectService(target: number) {}
 
-    handleBackButtonClick() {
-        this.setState({
-            target: 0,
-            section: 1,
-            isLoading: false,
-            categories: [defaultCategory]
-        }, () =>this.loadContent());
-        return true;
-    }
+  SearchFilterFunction(text: string) {
+    const newData = this.state.dataSoruce.filter(function (item) {
+      const itemData = item.name ? item.name.toUpperCase() : ''.toUpperCase();
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({
+      categories: newData,
+      search: text,
+    });
+  }
 
-    selectCategory (target: number){
-        this.setState({
-            target,
-            section: 2,
-            isLoading: false,
-            categories: []
-        }, () =>this.loadContent());
-    }
-    selectService(target: number){
-
-    }
-
-    SearchFilterFunction(text: string) {
-        const newData = this.state.dataSoruce.filter(function(item) {
-          const itemData = item.name ? item.name.toUpperCase() : ''.toUpperCase();
-          const textData = text.toUpperCase();
-          return itemData.indexOf(textData) > -1;
+  loadContent() {
+    const request =
+      this.state.section == 1
+        ? '/service-category/all'
+        : this.state.target == 0
+        ? '/service/all'
+        : '/service-category/' + this.state.target;
+    Axios.get(request, { params: { limit: 5 } })
+      .then((myJson) => {
+        const itemsArray = this.state.target != 0 ? myJson.data.data.services : myJson.data.data;
+        const newCategories = itemsArray.map((e: ListItem) => {
+          const formal = e.name.toUpperCase();
+          return {
+            name: formal,
+            id: e.id,
+          } as ListItem;
         });
-        this.setState({
-            categories: newData,
-            search: text
-        });
+        const categories = [...newCategories];
+        if (this.state.section == 1) categories.unshift(defaultCategory);
+        this.state.dataSoruce = categories;
+        this.setState({ categories, isLoading: false });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.state.dataSoruce = [];
+        this.setState({ categories: [], isLoading: false });
+      });
+  }
+
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackButtonClick);
+    this.loadContent();
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.handleBackButtonClick);
+  }
+
+  render() {
+    const target = 'Service';
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, paddingTop: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
     }
 
-    
-
-    loadContent(){
-        const request = (this.state.section==1)? 
-        '/service-category/all': (this.state.target==0)? 
-        '/service/all':'/service-category/'+this.state.target;;
-        Axios.get(request, { params: { limit: 5 } })
-        .then(myJson => {
-            const itemsArray = (this.state.target!=0)? myJson.data.data.services : myJson.data.data;
-            const newCategories = itemsArray.map((e: ListItem)=>{
-                const formal = e.name.toUpperCase();
-                return  {
-                    name: formal,
-                    id: e.id
-                } as ListItem
-            });
-            const categories = [...newCategories];
-            if(this.state.section==1)
-                categories.unshift(defaultCategory);
-            this.state.dataSoruce = categories;
-            this.setState({categories,
-                isLoading:false});
-        }).catch(err=>{
-            console.log(err);
-            this.state.dataSoruce = [];
-            this.setState({categories:[],
-                isLoading:false});
-        })
-    }
-
-    componentDidMount(){
-        BackHandler.addEventListener('hardwareBackPress', this.handleBackButtonClick);
-        this.loadContent();
-    }
-
-    componentWillUnmount(){
-        BackHandler.removeEventListener('hardwareBackPress', this.handleBackButtonClick);
-    }
-
-    render(){
-        const target = 'Service';
-        if(this.state.isLoading){
-            return(
-                <View style={{ flex: 1, paddingTop: 20 }}>
-                    <ActivityIndicator />
-                </View>
-            )
-        }
-
-       return(
-        <SafeAreaView style={styles.container} >
-            <View style={styles.searchHeader}>
-                <View style={(this.state.section==2)? styles.viewIcon:styles.viewNoIcon} >
-                    <Icon style={styles.icon}
-                    name={'chevron-left'} 
-                    onPress={ () => { this.handleBackButtonClick() } }  />
-                </View>
-                <View style={styles.searchBar}>
-                    <SearchBar 
-                    
-                    searchIcon={{ size: 24 }}
-                    onChangeText={text => this.SearchFilterFunction(text)}
-                    placeholder="Busca aqui..."
-                    value={this.state.search}
-                    />
-                </View>
-            </View>
-            <DirectoryScroll 
-                style={styles.directoryScroll}
-                image={(this.state.section==1)? BookMarkImage : CustomerSupport}
-                list={this.state.categories}
-                onPressItem={(this.state.section==1)? this.selectCategory: this.selectService}
-                key={this.state.section}
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searchHeader}>
+          <View style={this.state.section == 2 ? styles.viewIcon : styles.viewNoIcon}>
+            <Icon
+              style={styles.icon}
+              name={'chevron-left'}
+              onPress={() => {
+                this.handleBackButtonClick();
+              }}
             />
+          </View>
+          <View style={styles.searchBar}>
+            <SearchBar
+              searchIcon={{ size: 24 }}
+              onChangeText={(text) => this.SearchFilterFunction(text)}
+              placeholder="Busca aqui..."
+              value={this.state.search}
+            />
+          </View>
+        </View>
+        <DirectoryScroll
+          style={styles.directoryScroll}
+          image={this.state.section == 1 ? BookMarkImage : CustomerSupport}
+          list={this.state.categories}
+          onPressItem={this.state.section == 1 ? this.selectCategory : this.selectService}
+          key={this.state.section}
+        />
         <StatusBar style="auto" />
-        </SafeAreaView>
-       ) 
-    }
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    searchHeader:{
-        flexDirection: "row",
-        width: '100%',
-        flexWrap: 'wrap',
-    },
-    directoryScroll:{
-        width: '100%',
-        flexWrap: 'wrap',
-        flexDirection: 'column',
-    },
-    searchBar:{
-        flex: 1,
-        flexGrow: 8,
-        elevation: 4,
-    },
-    viewIcon:{
-        flex: 1,
-        flexGrow: 1,
-        backgroundColor: '#9e9e9e',
-        elevation: 4,
-    },
-    viewNoIcon:{
-        display: 'none'
-    },
-    icon:{
-        width: '100%',
-        height: '100%',
-        padding: 16
-    },
-    container: {
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-})
+  searchHeader: {
+    flexDirection: 'row',
+    width: '100%',
+    flexWrap: 'wrap',
+  },
+  directoryScroll: {
+    width: '100%',
+    flexWrap: 'wrap',
+    flexDirection: 'column',
+  },
+  searchBar: {
+    flex: 1,
+    flexGrow: 8,
+    elevation: 4,
+  },
+  viewIcon: {
+    flex: 1,
+    flexGrow: 1,
+    backgroundColor: '#9e9e9e',
+    elevation: 4,
+  },
+  viewNoIcon: {
+    display: 'none',
+  },
+  icon: {
+    width: '100%',
+    height: '100%',
+    padding: 16,
+  },
+  container: {
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
