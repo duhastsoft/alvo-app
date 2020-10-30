8aa9785d8aa787e4e814e59b9539d1e961350c5a
Kevin Velasquez <kevin.vela519@gmail.com>
2020-10-30 16:41:07 -0600

    Added config file example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Axios from 'axios';
 import { registerRootComponent } from 'expo';
 import React from 'react';
-import { Platform, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import env from './env';
 import Navigation from './navigation';
 
-Axios.defaults.baseURL = 'http://192.168.56.1:8080/api/v1';
+Axios.defaults.baseURL = env.API_URL;
 
 function App() {
   return (
@@ -16,13 +17,5 @@ function App() {
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  SafeArea: {
-    flex: 1,
-    backgroundColor: 'white',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-  },
-});
 
 export default registerRootComponent(App);
